d5fac5672b23ab382f4fb770eeea18ce24302d75
HallSolo <hall9727@gmail.com>
2017-02-15 17:26:04 +0300

    Добавлена прайс таблица

--- js/main.js
+++ js/main.js
@@ -1,10 +1,20 @@
 
+$('#cartype a').click(function (e) {
+    e.preventDefault();
+    console.log($(this));
+    $(this).tab('show');
+    $('li.nav-item>section').hide();
+    $(this).parent().children().show();
+})
+
 var Workspace = Backbone.Router.extend({
 
     routes: {
-        "main":           "main",    // #help
-        "service":        "service",  // #search/kiwis
-        "contacts":       "contacts"   // #search/kiwis/p7
+        "!/main":           "main",    // #help
+        "!/service/:car":   "service",  // #search/kiwis
+        "!/service":        "service",  // #search/kiwis
+        "!/contacts":       "contacts",   // #search/kiwis/p7
+        "":                 "main"
     },
 
     main: function() {
@@ -12,9 +22,16 @@ var Workspace = Backbone.Router.extend({
         $('#main').show();
     },
 
-    service: function() {
+    service: function(car) {
         $('#text>.container').children().hide();
         $('#service').show();
+        if(car!=''){
+            car = '#' + car;
+            $(document.body).append( $(car).prop('title') );
+            $(car +'>cartion>span').append($(car).prop('title'));
+            $('.bar').hide();
+            $(car).show();
+        }
     },
 
     contacts: function() {
